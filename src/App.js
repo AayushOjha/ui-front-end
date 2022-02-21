e53a3d9d53bca09dd08a3e8ebb6a548a917b0353
Ayush Ojha <18EGJCS024@GITJAIPUR.COM>
2022-02-21 13:29:40 +0530

    bohra meet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Box from '@mui/material/Box';
+import Boxes from './components/Boxes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar/>
+      <Box sx={{ height: '110vh', width:'90%', margin:'0 auto' }} >
+        <Boxes/>
+      </Box>
     </div>
   );
 }
